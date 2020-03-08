bfe6712959a3f3f3379cd61de51bab674ea75d71
CiderBeny <cider80@gmail.com>
2020-03-08 01:42:33 +0100

    jednostki file added

---
+++ jednostki.py
@@ -0,0 +1,11 @@
+cm = int(input("Podaj długość w cm: "))
+cm_to_m = cm/100
+cm_to_cal = cm/0.394
+
+print("Długość {} cm to {} metrów lub {:.3f} cali".format(cm, cm_to_m, cm_to_cal))
+
+kg = int(input ("Podaj wagę w kg:"))
+kg_to_gram = kg*100
+kg_to_funt = kg*2.20462
+print("Waga {} kg to {}gramów i {:.3f} funtów" .format(kg, kg_to_gram, kg_to_funt))
+
